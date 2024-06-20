1a52c62d259f9ee845773629dc33baa0f5361f2b
Amhjadkhan <amjadpogramer@gmail.com>
2024-06-20 13:48:38 -0500

    this is delete request commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -42,7 +42,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
   })
-
+//  delete request 
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+   }) 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
